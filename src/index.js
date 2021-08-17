97ea07dc69a5f1d16feefcd385750077ca45afbe
Nekoguard <nekoguard@protonmail.com>
2021-08-17 11:31:40 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let exprArr = expr.split(''),
+        wordArr = [],
+        letter = '',
+        word = '',
+        result;
+
+    for (let i = 0; i < exprArr.length; i += 10) {
+        for (let j = 0; j < 10; j += 2) {
+            for (let k = 0; k < 2; k++) {
+                letter += exprArr[i + j + k];
+            }
+
+            if (letter === '10') {
+                word += '.';
+            } else if (letter === '11') {
+                word += '-';
+            } else {
+                word += ' '
+            }
+
+            letter = '';
+        }
+        wordArr.push(MORSE_TABLE[word.trim()] || ' ');
+        word = '';
+    }
+
+    result = wordArr.join('');
+    return result;
 }
 
 module.exports = {
